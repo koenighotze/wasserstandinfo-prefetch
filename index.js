0b1ece712249549cac99d90e058465d742bde85b
David Schmitz <david.schmitz.privat@gmail.com>
2018-02-28 21:24:57 +0100

    Added diagnostics

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+
+global.lambdaContainerId = require('uuid4')()
+console.log('Setting up fresh instance', global.lambdaContainerId)
+
 const PegelOnline = require('./src/pegelOnline')
 const StationData = require('./src/stationData')
 const Util = require('util')
 
 exports.handler = (event, context, callback) => {
   console.log('Received ', Util.inspect(event))
+  console.log('Context ', Util.inspect(context))
+  console.log('Running as function', global.lambdaContainerId)
 
   console.log('Updating local station data')
   PegelOnline.fetchStations( (err, stations) => {
