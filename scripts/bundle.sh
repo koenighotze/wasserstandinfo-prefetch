544f63eb9ff2f4a2fc4a014ef61dfa818cac58e2
David <david.schmitz.privat@gmail.com>
2021-03-31 13:11:40 +0200

    Reduce noise in pipeline

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -8,12 +8,11 @@ APP_DEFAULT_VERSION=$(grep version package.json | cut -d: -f2 | sed 's/[", ]//g'
 
 temp_dir="temp-build-$$"
 mkdir "$temp_dir"
-cp -rvf src index.js package.json package-lock.json "$temp_dir" 
+cp -rf src index.js package.json package-lock.json "$temp_dir" 
 pushd "$temp_dir"
 npm ci --production -q
 zip -q -r -1 ../code.zip ./*
 popd
 
 echo "Uploading code in version ${APP_VERSION} to bucket ${BUCKET_NAME}"
-
-aws s3 cp code.zip "s3://${BUCKET_NAME}/${APP_VERSION}/"
+aws s3 cp --only-show-errors code.zip "s3://${BUCKET_NAME}/${APP_VERSION}/"
