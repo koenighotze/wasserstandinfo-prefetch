65e3b49dc257cc28db54d8c34f4f95c51ff70b1f
David <david.schmitz.privat@gmail.com>
2021-03-31 11:46:18 +0200

    Code is bundled and uploaded to s3

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -5,10 +5,6 @@ set -eu
 
 APP_VERSION=$(grep version package.json | cut -d: -f2 | sed 's/[", ]//g')
 
-npm run clean 
-npm test
-npm run lint
-
 temp_dir="temp-build-$$"
 mkdir "$temp_dir"
 cp -rvf src index.js package.json package-lock.json "$temp_dir" 
@@ -16,5 +12,7 @@ pushd "$temp_dir"
 npm ci --production -q
 zip -r -1 ../code.zip ./*
 popd
+
+echo "Uploading code in version ${APP_VERSION} to bucket ${BUCKET_NAME}"
 
 aws s3 cp code.zip "s3://${BUCKET_NAME}/${APP_VERSION}/"
