f9b261dbdc71563de7f4a1926c2e4e1b349f8a0d
David <david.schmitz.privat@gmail.com>
2021-04-08 18:55:34 +0200

    Autocleanup of code objects

--- provision/bootstrap/code-bucket.tf
+++ provision/bootstrap/code-bucket.tf
@@ -4,5 +4,26 @@ resource "aws_s3_bucket" "code" {
   acl           = "private"
   force_destroy = true
 
+  lifecycle_rule {
+    id      = "cleanup"
+    enabled = true
+
+    tags = local.common_tags
+
+    transition {
+      days          = 2
+      storage_class = "STANDARD_IA" 
+    }
+
+    transition {
+      days          = 4
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 10
+    }
+  }
+
   tags = local.common_tags
 }
