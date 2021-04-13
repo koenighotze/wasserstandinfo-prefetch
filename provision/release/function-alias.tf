5a9d546908686a9041ae4ed85cb68e3e23e00a94
David <david.schmitz.privat@gmail.com>
2021-04-13 11:19:40 +0200

    Alias ist basically the stage name

--- provision/release/function-alias.tf
+++ provision/release/function-alias.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_alias" "alias" {
-  name             = "wasserstandinfo-prefetch-${local.env_name}-alias"
-  description      = "Alias for Wasserstandinfo prefetch"
+  name             = local.env_name
+  description      = "Alias for Wasserstandinfo prefetch on ${local.env_name}"
   function_name    = data.terraform_remote_state.function.outputs.lambda_arn
   function_version = var.function_version
 }
